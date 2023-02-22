643d25521a4b58d8a91bac72807b9bc0eab32c7d
scott13579 <scott135@naver.com>
2023-02-22 16:09:07 +0900

    네비게이션 바 완성

--- app/src/main/java/ac/kr/tukorea/capstone_android/MainActivity.kt
+++ app/src/main/java/ac/kr/tukorea/capstone_android/MainActivity.kt
@@ -1,11 +1,40 @@
 package ac.kr.tukorea.capstone_android
 
+import ac.kr.tukorea.capstone_android.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+                R.id.home_menu -> replaceFragment(Home())
+                R.id.chat -> replaceFragment(Chat())
+                R.id.my_menu -> replaceFragment(MyMenu())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
     }
 }
