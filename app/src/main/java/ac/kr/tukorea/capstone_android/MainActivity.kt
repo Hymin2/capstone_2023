7c1a4089a2e0b8ed11aad95174f8604eb6ba1514
scott13579 <scott135@naver.com>
2023-02-23 18:54:52 +0900

    판매글 작성 페이지 제작중

--- app/src/main/java/ac/kr/tukorea/capstone_android/MainActivity.kt
+++ app/src/main/java/ac/kr/tukorea/capstone_android/MainActivity.kt
@@ -3,6 +3,8 @@ package ac.kr.tukorea.capstone_android
 import ac.kr.tukorea.capstone_android.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
@@ -10,10 +12,12 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        getSupportActionBar()?.hide()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(Home())
-
         binding.bottomNavigationView.setOnItemSelectedListener {
 
             when(it.itemId){
--- app/src/main/java/ac/kr/tukorea/capstone_android/WriteActivity.kt
+++ app/src/main/java/ac/kr/tukorea/capstone_android/WriteActivity.kt
@@ -2,10 +2,14 @@ package ac.kr.tukorea.capstone_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class WriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+
+        supportActionBar?.title = "판매글 작성"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
