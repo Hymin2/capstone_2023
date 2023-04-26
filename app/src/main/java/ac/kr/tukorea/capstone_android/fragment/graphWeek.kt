ec91e261e342f71b6499f1bbece3ca8bbefd9ba2
scott13579 <scott135@naver.com>
2023-04-26 23:43:26 +0900

    그래프

--- app/src/main/java/ac/kr/tukorea/capstone_android/fragment/graphWeek.kt
+++ app/src/main/java/ac/kr/tukorea/capstone_android/fragment/graphWeek.kt
@@ -3,21 +3,27 @@ package ac.kr.tukorea.capstone_android.fragment
 import ac.kr.tukorea.capstone_android.R
 import ac.kr.tukorea.capstone_android.data.GraphData
 import ac.kr.tukorea.capstone_android.databinding.FragmentGraphWeekBinding
+import android.content.Context
+import android.graphics.Canvas
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.HorizontalScrollView
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.MarkerView
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.DefaultValueFormatter
 import com.github.mikephil.charting.formatter.ValueFormatter
+import com.github.mikephil.charting.highlight.Highlight
 
 
 class graphWeek : Fragment() {
@@ -26,13 +32,31 @@ class graphWeek : Fragment() {
     private val binding get() = _binding!!
 
     val graphDataList: List<GraphData> = listOf(
-        GraphData("04-20",2),
-        GraphData("04-21",3),
-        GraphData("04-22",6),
-        GraphData("04-23",7),
-        GraphData("04-24",1),
-        GraphData("04-25",3),
-        GraphData("04-26",2)
+        GraphData("04-20",212323),
+        GraphData("04-21",323122),
+        GraphData("04-22",623121),
+        GraphData("04-23",74535),
+        GraphData("04-24",13345),
+        GraphData("04-25",36656),
+        GraphData("04-26",24545),
+        GraphData("04-20",22323),
+        GraphData("04-21",331221),
+        GraphData("04-22",6222),
+        GraphData("04-23",72312),
+        GraphData("04-24",112313),
+        GraphData("04-25",31232),
+        GraphData("04-26",233),
+        GraphData("04-20",2444),
+        GraphData("04-21",3555),
+        GraphData("04-22",6111),
+        GraphData("04-23",72222),
+        GraphData("04-24",13333),
+        GraphData("04-25",34444),
+        GraphData("04-26",25555),
+        GraphData("04-20",26666),
+        GraphData("04-21",37777),
+        GraphData("04-22",68888),
+        GraphData("04-23",7999),
     )
 
     override fun onCreateView(
@@ -60,13 +84,15 @@ class graphWeek : Fragment() {
         val lineDataSet = LineDataSet(entries,"entries")
 
         lineDataSet.apply {
-            color = resources.getColor(R.color.black,null)
-            circleRadius = 5f
-            lineWidth = 3f
-            setCircleColor(resources.getColor(R.color.purple_700, null))
-            circleHoleColor = resources.getColor(R.color.purple_700, null)
-            setDrawHighlightIndicators(false)
-            setDrawValues(true) // 숫자표시
+            color = resources.getColor(android.R.color.holo_red_dark,null)
+            setDrawCircles(true)
+            circleRadius = 2f
+            lineWidth = 1f
+            setCircleColor(resources.getColor(android.R.color.holo_red_dark, null))
+            circleHoleColor = resources.getColor(R.color.white, null)
+            setDrawHighlightIndicators(true)
+            highLightColor = resources.getColor(android.R.color.holo_red_dark, null)
+            setDrawValues(false) // 숫자표시
             valueTextColor = resources.getColor(R.color.black, null)
             valueFormatter = DefaultValueFormatter(0)  // 소숫점 자릿수 설정
             valueTextSize = 10f
@@ -80,18 +106,21 @@ class graphWeek : Fragment() {
             isDragXEnabled = true   // x 축 드래그 여부
             isScaleYEnabled = false //y축 줌 사용여부
             isScaleXEnabled = false //x축 줌 사용여부
+            setDrawMarkers(true)
+            val customMarkerView = CustomMarkerView(context, layoutResource = R.layout.graphmarker)
+            marker = customMarkerView
         }
 
         xAxis.apply {
             setDrawGridLines(false)
-            setDrawAxisLine(true)
-            setDrawLabels(true)
+            setDrawAxisLine(false)
+            setDrawLabels(false)
             position = XAxis.XAxisPosition.BOTTOM
             valueFormatter = XAxisCustomFormatter(changeDateText(graphDataList))
             textColor = resources.getColor(R.color.black, null)
             textSize = 10f
             labelRotationAngle = 0f
-            setLabelCount(10, true)
+            setLabelCount(100, true)
         }
         val horizontalScrollView = view.findViewById<HorizontalScrollView>(R.id.graphWeek_scrollview)
         horizontalScrollView.post{
@@ -106,9 +135,7 @@ class graphWeek : Fragment() {
             notifyDataSetChanged() //데이터 갱신
             invalidate() // view갱신
         }
-
         lineChart
-
     }
 
     fun changeDateText(dataList: List<GraphData>): List<String> {
@@ -129,6 +156,28 @@ class graphWeek : Fragment() {
 
         override fun getFormattedValue(value: Float): String {
             return xAxisData[(value).toInt()]
+        }
+
+    }
+
+    class CustomMarkerView : MarkerView {
+        private var tvContent: TextView
+        // marker
+        constructor(context: Context?, layoutResource: Int) : super(context, layoutResource) {
+            tvContent = findViewById(R.id.tvContent)
+        }
+
+        // draw override를 사용해 marker의 위치 조정 (bar의 상단 중앙)
+        override fun draw(canvas: Canvas?) {
+            canvas!!.translate(-(width/100).toFloat(), -(height).toFloat() )
+            super.draw(canvas)
+        }
+
+        // entry를 content의 텍스트에 지정
+        override fun refreshContent(e: Entry?, highlight: Highlight?) {
+            Log.e("asd","$e.x")
+            tvContent.text = e?.y?.toInt().toString() + "원\n"
+            super.refreshContent(e, highlight)
         }
 
     }
